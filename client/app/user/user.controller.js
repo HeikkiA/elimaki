c6e53d69b964e9e92a473e7c76bd73903bcb0c68
Heikki Ahlgren <heikki.ahlgren@sc5.io>
2015-05-05 12:50:31 +0300

    Deleted signup view, moved user addition to user view

--- client/app/user/user.controller.js
+++ client/app/user/user.controller.js
@@ -1,15 +1,35 @@
+/* global confirm */
 'use strict';
 
 angular.module('kulutApp')
   .controller('UserCtrl', function ($scope, $http, socket, Auth, User) {
+    $scope.user = {};
     $scope.users = [];
     $scope.sort = { by: 'name', reverse: false };
-    $scope.currentUser = Auth.getCurrentUser();
+    $scope.isAdmin = Auth.isAdmin;
 
     $http.get('/api/users').success(function(users) {
       $scope.users = users;
       socket.syncUpdates('user', $scope.users);
     });
+
+    $scope.register = function(form) {
+      if (form.$valid) {
+        Auth.createUser({
+          name: $scope.user.displayName,
+          realName: $scope.user.realName,
+          email: $scope.user.email,
+          iban: $scope.user.iban,
+          password: $scope.user.password,
+        })
+        .then( function() {
+          $scope.user = {};
+        })
+        .catch(function(err) {
+          console.log('Error creating new user:', err);
+        });
+      }
+    };
 
     $scope.delete = function(user) {
       if (confirm('Really delete this user?')) {
