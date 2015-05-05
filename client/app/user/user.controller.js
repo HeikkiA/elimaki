b5f963dcc323c41e880381dfaf489e61bd2003fb
Heikki Ahlgren <heikki.ahlgren@sc5.io>
2015-05-05 12:38:15 +0300

    Deleted admin view, moved user deletion in user view

--- client/app/user/user.controller.js
+++ client/app/user/user.controller.js
@@ -1,14 +1,26 @@
 'use strict';
 
 angular.module('kulutApp')
-  .controller('UserCtrl', function ($scope, $http, socket) {
+  .controller('UserCtrl', function ($scope, $http, socket, Auth, User) {
     $scope.users = [];
     $scope.sort = { by: 'name', reverse: false };
+    $scope.currentUser = Auth.getCurrentUser();
 
     $http.get('/api/users').success(function(users) {
       $scope.users = users;
       socket.syncUpdates('user', $scope.users);
     });
+
+    $scope.delete = function(user) {
+      if (confirm('Really delete this user?')) {
+        User.remove({ id: user._id });
+        angular.forEach($scope.users, function(u, i) {
+          if (u === user) {
+            $scope.users.splice(i, 1);
+          }
+        });
+      }
+    };
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('user');
