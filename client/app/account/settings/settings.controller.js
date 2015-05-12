9f290c2b6e0dd910eafb484800dcc6ecfa18013d
Heikki Ahlgren <heikki.ahlgren@sc5.io>
2015-05-12 11:58:26 +0300

    Revised password changing

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -1,21 +1,38 @@
 'use strict';
 
 angular.module('kulutApp')
-  .controller('SettingsCtrl', function ($scope, User, Auth) {
-    $scope.errors = {};
+  .controller('SettingsCtrl', function ($scope, $location, Auth) {
 
     $scope.changePassword = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
-      }
+      Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
+      .then(function() {
+        $location.path('/');
+      })
+      .catch(function(err) {
+        console.log('error changing password:', err.data);
+      });
 		};
+
+    $scope.checkNewPassword = function() {
+      console.log($scope.user);
+
+      $scope.passwordForm.newPassword.$setValidity('', true);
+      $scope.passwordForm.newPassword2.$setValidity('', true);
+
+      if ($scope.user.newPassword && $scope.user.newPassword.length < 3) {
+        $scope.passwordForm.newPassword.$setValidity('', false);
+      }
+
+      if ($scope.user.newPassword2 && $scope.user.newPassword2.length < 3) {
+        $scope.passwordForm.newPassword2.$setValidity('', false);
+      }
+
+      if ($scope.user.newPassword && $scope.user.newPassword2 &&
+          $scope.user.newPassword.length > 2 && $scope.user.newPassword2.length > 2 &&
+          $scope.user.newPassword !== $scope.user.newPassword2) {
+        $scope.passwordForm.newPassword.$setValidity('', false);
+        $scope.passwordForm.newPassword2.$setValidity('', false);
+      }
+    };
+
   });
