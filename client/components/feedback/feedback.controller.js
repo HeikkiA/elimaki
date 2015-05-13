3f0780db7cabafd1310cadadec87e5f370a14947
Heikki Ahlgren <heikki.ahlgren@sc5.io>
2015-05-13 08:56:49 +0300

    Close alerts after timeout

--- client/components/feedback/feedback.controller.js
+++ client/components/feedback/feedback.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('kulutApp')
-  .controller('FeedbackCtrl', function ($scope) {
+  .controller('FeedbackCtrl', function ($scope, $timeout) {
     $scope.alerts = [];
 
     $scope.closeAlert = function(index) {
@@ -9,7 +9,14 @@ angular.module('kulutApp')
     };
 
     $scope.addAlert = function(type, msg) {
+      // show maximum of three latest alarms
+      if ($scope.alerts.length > 2) {
+        $scope.alerts.splice(0, $scope.alerts.length - 2);
+      }
       $scope.alerts.push({ type: type, msg: msg });
+      $timeout(function() {
+        $scope.closeAlert(0);
+      }, 5000);
     };
 
   });
